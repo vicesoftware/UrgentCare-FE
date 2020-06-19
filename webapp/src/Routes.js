54a70871dcc3140b656d514292238cde6b9d67d5
Sakshi Panwar <sakshi.panwar@vicesoftware.com>
2020-06-19 19:44:06 +0530

    made provider visit page

--- webapp/src/Routes.js
+++ webapp/src/Routes.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Page from './widgets/Page/Page'
 import VisitsArchive from './features/VisitsArchive'
+import VisitsProvider from './features/VisitsProvider'
+
 export default function Routes() {
 	return (
 		<Switch>
+			<PageRoute path='/provider'>
+				<VisitsProvider />
+			</PageRoute>
 			<PageRoute path='/'>
 				<VisitsArchive />
 			</PageRoute>
